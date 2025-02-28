e1365346bd6eeb36fb7d23ac96c0276970778233
maxplush <maxabeplush@gmail.com>
2025-02-28 10:53:34 -0800

    prolem 6

--- sql/06.sql
+++ sql/06.sql
@@ -3,3 +3,9 @@
  * HINT:
  * Use the count() aggregation function and group by the actor's last name.
  */
+
+SELECT last_name, COUNT(*) AS actor_count
+FROM actor
+GROUP BY last_name
+ORDER BY actor_count DESC, last_name ASC;
+
